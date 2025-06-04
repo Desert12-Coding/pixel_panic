f8d960f3c9130dfdd767629994ef4417ece72b6c
gaymecockring <jame.cotchong@gmail.com>
2025-06-05 10:02:16 +1200

    furta

---
+++ pixel_panic_0.1/Assets/OneTimeBreakableIce.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class OneTimeBreakableIce : MonoBehaviour
+{
+    public ParticleSystem breakEffect;
+    public AudioClip breakSound;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Snowball"))
+        {
+            BreakPlatform();
+        }
+    }
+
+    private void BreakPlatform()
+    {
+        // Play effects
+        if (breakEffect != null) Instantiate(breakEffect, transform.position, Quaternion.identity);
+        if (breakSound != null) AudioSource.PlayClipAtPoint(breakSound, transform.position);
+        
+        // Disable platform
+        gameObject.SetActive(false);
+        
+        // Alternatively, destroy completely:
+        // Destroy(gameObject);
+    }
+}
