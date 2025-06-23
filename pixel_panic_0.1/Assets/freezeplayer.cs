f1832b22b4ff0823f588b566f30738c87cb34cea
gaymecockring <jame.cotchong@gmail.com>
2025-06-24 11:58:45 +1200

    hh

--- pixel_panic_0.1/Assets/freezeplayer.cs
+++ pixel_panic_0.1/Assets/freezeplayer.cs
@@ -5,7 +5,22 @@ public class freezeplayer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        [header("Freeze settings")]
+        public int requiredPresses = 6;
+        public float freezeInterval = 10f;
+
+        [header("color changer")]
+        public Color frozencolor = new color(0.6f, 0.9f, 1f); 
         
+        [Header("UI")]
+        public UnityEngine.UI.Text pressCounterText;
+
+        private int currentPressCount = 0;
+        Private int Rigidbody2D;
+        Private SpriteRenderer spriteRenderer;
+        Private bool isFrozen = false;
+        Private float Timer = 0f;
+        private playerInput PlayerInput;
     }
 
     // Update is called once per frame
